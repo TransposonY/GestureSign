6541d711cc9e6f7a8d8161152f72818294498640
TransposonY <ccfyzh@gmail.com>
2015-03-04 16:23:50 +0800

    fix PointInfo NullReferenceException

--- GestureSign.Common/Plugins/PointInfo.cs
+++ GestureSign.Common/Plugins/PointInfo.cs
@@ -6,50 +6,51 @@ using System.Drawing;
 
 namespace GestureSign.Common.Plugins
 {
-	public class PointInfo
-	{
-		#region Private Variables
+    public class PointInfo
+    {
+        #region Private Variables
 
-		private Point _TouchLocation;
-		private IntPtr _WindowHandle;
-		private ManagedWinapi.Windows.SystemWindow _Window;
+        private Point _TouchLocation;
+        private IntPtr _WindowHandle;
+        private ManagedWinapi.Windows.SystemWindow _Window;
 
-		#endregion
+        #endregion
 
-		#region Constructors
+        #region Constructors
 
-		public PointInfo(PointF TouchLocation)
-		{
-			_TouchLocation = Point.Round(TouchLocation);
-			_Window = ManagedWinapi.Windows.SystemWindow.FromPointEx(_TouchLocation.X, _TouchLocation.Y, true, false);
-			_WindowHandle = _Window.HWnd;
-		}
+        public PointInfo(PointF TouchLocation)
+        {
+            _TouchLocation = Point.Round(TouchLocation);
+            _Window = ManagedWinapi.Windows.SystemWindow.FromPointEx(_TouchLocation.X, _TouchLocation.Y, true, false);
+            if (_Window == null) _WindowHandle = IntPtr.Zero;
+            else _WindowHandle = _Window.HWnd;
+        }
 
-		#endregion
+        #endregion
 
-		#region Public Properties
+        #region Public Properties
 
-		public Point TouchLocation
-		{
-			get { return _TouchLocation; }
-			set
-			{
-				_TouchLocation = value;
-				_Window = ManagedWinapi.Windows.SystemWindow.FromPointEx(value.X, value.Y, true, false);
-				_WindowHandle = _Window.HWnd;
-			}
-		}
+        public Point TouchLocation
+        {
+            get { return _TouchLocation; }
+            set
+            {
+                _TouchLocation = value;
+                _Window = ManagedWinapi.Windows.SystemWindow.FromPointEx(value.X, value.Y, true, false);
+                _WindowHandle = _Window.HWnd;
+            }
+        }
 
-		public IntPtr WindowHandle
-		{
-			get { return _WindowHandle; }
-		}
+        public IntPtr WindowHandle
+        {
+            get { return _WindowHandle; }
+        }
 
-		public ManagedWinapi.Windows.SystemWindow Window
-		{
-			get { return _Window; }
-		}
+        public ManagedWinapi.Windows.SystemWindow Window
+        {
+            get { return _Window; }
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
