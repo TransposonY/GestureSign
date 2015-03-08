3fbf64f99bc6b83b16df6327b395b263da2ba9e6
TransposonY <ccfyzh@gmail.com>
2015-03-08 13:17:25 +0800

    Minimize:Exclude modern app

--- GestureSign.CorePlugins/Minimize.cs
+++ GestureSign.CorePlugins/Minimize.cs
@@ -9,88 +9,92 @@ using System.Windows.Controls;
 
 namespace GestureSign.CorePlugins
 {
-	public class Minimize : IPlugin
-	{
-		#region Private Variables
+    public class Minimize : IPlugin
+    {
+        #region Private Variables
 
-		IHostControl _HostControl = null;
+        IHostControl _HostControl = null;
 
-		#endregion
+        #endregion
 
-		#region Public Properties
+        #region Public Properties
 
-		public string Name
-		{
-			get { return "最小化"; }
-		}
+        public string Name
+        {
+            get { return "最小化"; }
+        }
 
-		public string Description
-		{
+        public string Description
+        {
             get { return "最小化当前窗口"; }
-		}
+        }
 
-		public UserControl GUI
-		{
-			get { return null; }
-		}
+        public UserControl GUI
+        {
+            get { return null; }
+        }
 
-		public string Category
-		{
-			get { return "Windows"; }
-		}
+        public string Category
+        {
+            get { return "Windows"; }
+        }
 
-		public bool IsAction
-		{
-			get { return true; }
-		}
+        public bool IsAction
+        {
+            get { return true; }
+        }
 
-		#endregion
+        #endregion
 
-		#region Public Methods
+        #region Public Methods
 
-		public void Initialize()
-		{
+        public void Initialize()
+        {
 
-		}
+        }
 
-		public void ShowGUI(bool IsNew)
-		{
-			// Nothing to do here
-		}
+        public void ShowGUI(bool IsNew)
+        {
+            // Nothing to do here
+        }
 
-		public bool Gestured(Common.Plugins.PointInfo ActionPoint)
-		{
-			// Don't attempt to minimize tool windows (including Windows Program Manager)
-			if ((ActionPoint.Window.ExtendedStyle & WindowExStyleFlags.TOOLWINDOW) == WindowExStyleFlags.TOOLWINDOW)
-				return false;
+        public bool Gestured(Common.Plugins.PointInfo ActionPoint)
+        {
+            try
+            {
+                // Don't attempt to minimize tool windows (including Windows Program Manager)
+                if (ActionPoint.Window.ClassName == "Windows.UI.Core.CoreWindow" ||
+                    (ActionPoint.Window.ExtendedStyle & WindowExStyleFlags.TOOLWINDOW) == WindowExStyleFlags.TOOLWINDOW)
+                    return false;
 
-			// Minimize window
-			ActionPoint.Window.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+                // Minimize window
+                ActionPoint.Window.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            }
+            catch { return false; }
+            return true;
+        }
 
-			return true;
-		}
+        public void Deserialize(string SerializedData)
+        {
+            // Nothing to do here
+        }
 
-		public void Deserialize(string SerializedData)
-		{
-			// Nothing to do here
-		}
+        public string Serialize()
+        {
+            // Nothing to serialize
+            return "";
+        }
 
-		public string Serialize()
-		{
-			// Nothing to serialize
-			return "";
-		}
+        #endregion
 
-		#endregion
+        #region Host Control
 
-		#region Host Control
+        public IHostControl HostControl
+        {
+            get { return _HostControl; }
+            set { _HostControl = value; }
+        }
 
-		public IHostControl HostControl
-		{
-			get { return _HostControl; }
-			set { _HostControl = value; }
-		}
-
-		#endregion
-	}
+        #endregion
+    }
 }
--- GestureSign/Properties/AssemblyInfo.cs
+++ GestureSign/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.*")]
-[assembly: AssemblyFileVersion("0.8.9.0")]
+[assembly: AssemblyVersion("0.9.*")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
