b2bd4d98a9eceb16d1ac26de516493433efb0666
TransposonY <ccfyzh@gmail.com>
2015-03-26 13:26:57 +0800

    IO:add using()

--- GestureSign.Common/Configuration/FileManager.cs
+++ GestureSign.Common/Configuration/FileManager.cs
@@ -44,12 +44,14 @@ namespace GestureSign.Common.Configuration
                 DataContractJsonSerializer jSerial = KnownTypes != null ? new DataContractJsonSerializer(typeof(T), KnownTypes) : new DataContractJsonSerializer(typeof(T));
 
                 // Open json file
-                StreamWriter sWrite = new StreamWriter(filePath);
+                using (StreamWriter sWrite = new StreamWriter(filePath))
+                {
 
-                // Serialize actions into json file
-                jSerial.WriteObject(sWrite.BaseStream, SerializableObject);
-                // Close file
-                sWrite.Close();
+                    // Serialize actions into json file
+                    jSerial.WriteObject(sWrite.BaseStream, SerializableObject);
+                    // Close file
+                    sWrite.Close();
+                }
                 mutex.ReleaseMutex();
                 return true;
             }
@@ -67,21 +69,24 @@ namespace GestureSign.Common.Configuration
             {
                 mutex.WaitOne();
                 if (!File.Exists(filePath)) return default(T);
-                StreamReader sRead = new StreamReader(filePath);
-                int BOM = sRead.BaseStream.ReadByte();
-                if (BOM == 0xEF)
-                    sRead.BaseStream.Seek(3, SeekOrigin.Begin);
-                else sRead.BaseStream.Seek(0, SeekOrigin.Begin);
-                // Create json serializer to deserialize json file
-                DataContractJsonSerializer jSerial = KnownTypes != null ? new DataContractJsonSerializer(typeof(T), KnownTypes) : new DataContractJsonSerializer(typeof(T));
+                using (StreamReader sRead = new StreamReader(filePath))
+                {
+                    int BOM = sRead.BaseStream.ReadByte();
+                    if (BOM == 0xEF)
+                        sRead.BaseStream.Seek(3, SeekOrigin.Begin);
+                    else sRead.BaseStream.Seek(0, SeekOrigin.Begin);
+                    // Create json serializer to deserialize json file
+                    DataContractJsonSerializer jSerial = KnownTypes != null ? new DataContractJsonSerializer(typeof(T), KnownTypes) : new DataContractJsonSerializer(typeof(T));
 
-                // Deserialize json file into actions list
-                T objBuffer = (T)jSerial.ReadObject(sRead.BaseStream);
+                    // Deserialize json file into actions list
+                    T objBuffer = (T)jSerial.ReadObject(sRead.BaseStream);
 
-                sRead.Close();
-                mutex.ReleaseMutex();
-                // Return results of serialization
-                return objBuffer;
+                    sRead.Close();
+
+                    mutex.ReleaseMutex();
+                    // Return results of serialization
+                    return objBuffer;
+                }
             }
             catch (System.Runtime.Serialization.SerializationException)
             {
