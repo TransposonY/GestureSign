fa9c0c22846b8c85773de132ee756fc4009537bf
TransposonY <ccfyzh@gmail.com>
2015-05-22 14:35:15 +0800

    Add plugin: Delay

---
+++ GestureSign.CorePlugins/Delay/Delay.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using GestureSign.Common.Plugins;
+
+using System.Windows.Controls;
+
+namespace GestureSign.CorePlugins.Delay
+{
+    public class Delay : IPlugin
+    {
+        #region IPlugin Instance Fields
+
+        private DelayUI _GUI = null;
+        private int _timeout;
+
+        #endregion
+
+        #region IPlugin Instance Properties
+
+        public string Name
+        {
+            get { return "延时"; }
+        }
+
+        public string Category
+        {
+            get { return "GestureSign"; }
+        }
+
+        public string Description
+        {
+            get { return String.Format("延迟 {0} 毫秒", _timeout); }
+        }
+
+        public bool IsAction
+        {
+            get { return true; }
+        }
+
+        public UserControl GUI
+        {
+            get
+            {
+                if (_GUI == null)
+                    _GUI = CreateGUI();
+
+                return _GUI;
+            }
+        }
+
+        public DelayUI TypedGUI
+        {
+            get { return (DelayUI)GUI; }
+        }
+
+        public IHostControl HostControl { get; set; }
+
+        #endregion
+
+        #region IPlugin Instance Methods
+
+        public void Initialize()
+        {
+
+        }
+
+        public bool Gestured(PointInfo ActionPoint)
+        {
+            try
+            {
+                System.Threading.Thread.Sleep(_timeout);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Deserialize(string SerializedData)
+        {
+            return int.TryParse(SerializedData, out _timeout);
+        }
+
+        public string Serialize()
+        {
+            if (_GUI != null)
+            {
+                _timeout = _GUI.Timeout;
+            }
+            return _timeout.ToString();
+        }
+
+        #endregion
+
+        #region Private Instance Methods
+
+        private DelayUI CreateGUI()
+        {
+            DelayUI delayUI = new DelayUI();
+            delayUI.Loaded += (s, o) =>
+            {
+                TypedGUI.Timeout = _timeout;
+            };
+            return delayUI;
+        }
+
+        #endregion
+    }
+}
---
+++ GestureSign.CorePlugins/Delay/DelayUI.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestureSign.CorePlugins.Delay
+{
+    /// <summary>
+    /// DelayUI.xaml 的交互逻辑
+    /// </summary>
+    public partial class DelayUI : UserControl
+    {
+        private int _timeout;
+        public DelayUI()
+        {
+            InitializeComponent();
+        }
+
+        public int Timeout
+        {
+            get
+            {
+                int timeout;
+                bool flag = int.TryParse(TimeoutTextBox.Text, out timeout);
+                _timeout = flag ? timeout : 0;
+                return _timeout;
+            }
+            set
+            {
+                _timeout = value;
+                TimeoutTextBox.Text = _timeout.ToString();
+            }
+        }
+        private void TimeoutTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || e.Key == Key.Decimal)
+            {
+                if (txt != null && (txt.Text.Contains(".") && e.Key == Key.Decimal))
+                {
+                    e.Handled = true;
+                    return;
+                }
+                e.Handled = false;
+            }
+            else if (((e.Key >= Key.D0 && e.Key <= Key.D9) || e.Key == Key.OemPeriod) && e.KeyboardDevice.Modifiers != ModifierKeys.Shift)
+            {
+                if (txt != null && (txt.Text.Contains(".") && e.Key == Key.OemPeriod))
+                {
+                    e.Handled = true;
+                    return;
+                }
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TimeoutTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            var change = new TextChange[e.Changes.Count];
+            e.Changes.CopyTo(change, 0);
+
+            int offset = change[0].Offset;
+            if (change[0].AddedLength > 0)
+            {
+                int num = 0;
+                if (textBox == null || int.TryParse(textBox.Text, out num))
+                {
+                    if (num < 0)
+                    {
+                        if (textBox != null) textBox.Text = 0.ToString();
+                    }
+                    //else if (num > 10000) if (textBox != null) textBox.Text = 10000.ToString();
+                    return;
+                }
+                textBox.Text = textBox.Text.Remove(offset, change[0].AddedLength);
+                textBox.Select(offset, 0);
+            }
+        }
+    }
+}
