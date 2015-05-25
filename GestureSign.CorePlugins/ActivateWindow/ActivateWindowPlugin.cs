099c297a4f7cf1e0505bfc6e6042a4ba28d55d7b
TransposonY <ccfyzh@gmail.com>
2015-05-25 12:38:07 +0800

    New Plugin:ActivateWindow

---
+++ GestureSign.CorePlugins/ActivateWindow/ActivateWindowPlugin.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Controls;
+using GestureSign.Common.Plugins;
+using ManagedWinapi.Windows;
+
+namespace GestureSign.CorePlugins.ActivateWindow
+{
+    public class ActivateWindowPlugin : IPlugin
+    {
+        #region Private Variables
+
+        private ActivateWindowUI _gui;
+        private ActivateWindowSettings _settings;
+        private delegate bool CallBackEnumWindowsProc(IntPtr hWnd, int lParam);
+
+        private bool _isFound;
+
+        private const string User32 = "user32.dll";
+        #endregion
+
+        #region PInvoke Declarations
+        [DllImport(User32)]
+        private static extern int EnumWindows(CallBackEnumWindowsProc ewp, int lParam);
+
+        [DllImport(User32)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+        [DllImport(User32)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        #endregion
+
+        #region Public Properties
+
+        public string Name
+        {
+            get { return "激活窗口"; }
+        }
+
+        public string Description
+        {
+            get { return "使指定窗口前置"; }
+        }
+
+        public UserControl GUI
+        {
+            get { return _gui ?? (_gui = CreateGUI()); }
+        }
+
+        public ActivateWindowUI TypedGUI
+        {
+            get { return (ActivateWindowUI)GUI; }
+        }
+
+        public string Category
+        {
+            get { return "Windows"; }
+        }
+
+        public bool IsAction
+        {
+            get { return true; }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Initialize()
+        {
+
+        }
+
+        public bool Gestured(PointInfo actionPoint)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            do
+            {
+                if (_settings.IsRegEx)
+                {
+                    CallBackEnumWindowsProc ewp = EnumWindowsProc;
+                    EnumWindows(ewp, 0);
+                    if (_isFound) return true;
+                }
+                else
+                {
+                    string className = String.IsNullOrWhiteSpace(_settings.ClassName) ? null : _settings.ClassName;
+                    string caption = String.IsNullOrWhiteSpace(_settings.Caption) ? null : _settings.Caption;
+                    IntPtr hWnd = FindWindow(className, caption);
+                    if (hWnd != IntPtr.Zero)
+                    {
+                        SystemWindow.ForegroundWindow = new SystemWindow(hWnd);
+                        sw.Stop();
+                        return true;
+                    }
+                }
+                Thread.Sleep(10);
+            } while (sw.ElapsedMilliseconds < _settings.Timeout);
+            sw.Stop();
+            return false;
+        }
+
+        private bool EnumWindowsProc(IntPtr hWnd, int lParam)
+        {
+            if (IsWindowVisible(hWnd))
+            {
+                try
+                {
+                    var window = new SystemWindow(hWnd);
+
+                    if (
+                        Regex.IsMatch(window.Title, _settings.Caption,
+                        RegexOptions.Singleline | RegexOptions.IgnoreCase) &&
+                        Regex.IsMatch(window.ClassName, _settings.ClassName,
+                        RegexOptions.Singleline | RegexOptions.IgnoreCase))
+                    {
+                        _isFound = true;
+                        SystemWindow.ForegroundWindow = window;
+                        return false;
+                    }
+                }
+                catch
+                {
+                    _isFound = true;
+                    return false;
+                }
+            }
+            _isFound = false;
+            return true;
+        }
+
+        public bool Deserialize(string SerializedData)
+        {
+            // Clear existing settings if nothing was passed in
+            if (String.IsNullOrEmpty(SerializedData))
+            {
+                _settings = new ActivateWindowSettings();
+                return true;
+            }
+            try
+            {
+                // Create memory stream from serialized data string
+                MemoryStream memStream = new MemoryStream(Encoding.Default.GetBytes(SerializedData));
+
+                // Create json serializer to deserialize json file
+                DataContractJsonSerializer jSerial = new DataContractJsonSerializer(typeof(ActivateWindowSettings));
+
+                // Deserialize json file into actions list
+                _settings = jSerial.ReadObject(memStream) as ActivateWindowSettings;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            if (_settings == null)
+                _settings = new ActivateWindowSettings();
+            return true;
+        }
+
+        public string Serialize()
+        {
+            if (_gui != null)
+                _settings = _gui.Settings;
+
+            if (_settings == null)
+                _settings = new ActivateWindowSettings();
+
+            // Create json serializer to serialize json file
+            DataContractJsonSerializer jSerial = new DataContractJsonSerializer(typeof(ActivateWindowSettings));
+
+            // Open json file
+            MemoryStream mStream = new MemoryStream();
+
+            // Serialize actions into json file
+            jSerial.WriteObject(mStream, _settings);
+
+            return Encoding.Default.GetString(mStream.ToArray());
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private ActivateWindowUI CreateGUI()
+        {
+            ActivateWindowUI newGUI = new ActivateWindowUI() { Settings = _settings };
+            newGUI.Loaded += (o, e) => { TypedGUI.Settings = _settings; };
+
+            return newGUI;
+        }
+
+        #endregion
+
+        #region Host Control
+
+        public IHostControl HostControl { get; set; }
+
+        #endregion
+    }
+}
---
+++ GestureSign.CorePlugins/ActivateWindow/ActivateWindowUI.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestureSign.CorePlugins.ActivateWindow
+{
+    /// <summary>
+    /// ActivateWindowUI.xaml 的交互逻辑
+    /// </summary>
+    public partial class ActivateWindowUI : UserControl
+    {
+        public ActivateWindowUI()
+        {
+            InitializeComponent();
+        }
+
+        public ActivateWindowSettings Settings
+        {
+            get
+            {
+                int timeout = 0;
+                int.TryParse(TimeoutTextBox.Text, out timeout);
+                return new ActivateWindowSettings()
+                {
+                    ClassName = ClassNameTextBox.Text.Trim(),
+                    Caption = CaptionTextBox.Text.Trim(),
+                    IsRegEx = IsRegExCheckBox.IsChecked.Value,
+                    Timeout = timeout
+                };
+            }
+            set
+            {
+                ClassNameTextBox.Text = value.ClassName;
+                CaptionTextBox.Text = value.Caption;
+                IsRegExCheckBox.IsChecked = value.IsRegEx;
+                TimeoutTextBox.Text = value.Timeout.ToString();
+            }
+        }
+
+        private void TimeoutTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || e.Key == Key.Decimal)
+            {
+                if (txt != null && (txt.Text.Contains(".") && e.Key == Key.Decimal))
+                {
+                    e.Handled = true;
+                    return;
+                }
+                e.Handled = false;
+            }
+            else if (((e.Key >= Key.D0 && e.Key <= Key.D9) || e.Key == Key.OemPeriod) && e.KeyboardDevice.Modifiers != ModifierKeys.Shift)
+            {
+                if (txt != null && (txt.Text.Contains(".") && e.Key == Key.OemPeriod))
+                {
+                    e.Handled = true;
+                    return;
+                }
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TimeoutTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            var change = new TextChange[e.Changes.Count];
+            e.Changes.CopyTo(change, 0);
+
+            int offset = change[0].Offset;
+            if (change[0].AddedLength > 0)
+            {
+                int num = 0;
+                if (textBox == null || int.TryParse(textBox.Text, out num))
+                {
+                    if (num < 0)
+                    {
+                        if (textBox != null) textBox.Text = 0.ToString();
+                    }
+                    return;
+                }
+                textBox.Text = textBox.Text.Remove(offset, change[0].AddedLength);
+                textBox.Select(offset, 0);
+            }
+        }
+    }
+}
