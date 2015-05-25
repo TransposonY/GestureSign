9210c3017048786888359e567357d140e809c29d
TransposonY <ccfyzh@gmail.com>
2015-05-25 21:28:56 +0800

    version 0.11.2

--- GestureSign/Properties/AssemblyInfo.cs
+++ GestureSign/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.11.*")]
-[assembly: AssemblyFileVersion("0.11.1.0")]
+[assembly: AssemblyFileVersion("0.11.2.0")]
