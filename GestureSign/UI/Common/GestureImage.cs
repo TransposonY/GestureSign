8038346c6a286efc1974a8ad2760b8e4c7b2bbe3
TransposonY <ccfyzh@gmail.com>
2015-07-06 00:02:44 +0800

    rename

--- GestureSign/UI/Common/GestureImage.cs
+++ GestureSign/UI/Common/GestureImage.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows;
-
-using System.Management;
+using System.Windows.Media;
 
 namespace GestureSign.UI.Common
 {
@@ -19,48 +13,46 @@ namespace GestureSign.UI.Common
         // static Point Dpi = GetPixelsPerXLogicalInch();
         #endregion
 
-        private static Point[] ScaleGesture(List<System.Drawing.Point> Input, double Width, double Height, out Size _ScaledSize)
+        private static Point[] ScaleGesture(List<System.Drawing.Point> input, double width, double height, out Size scaledSize)
         {
             // Create generic list of points to hold scaled stroke
-            List<Point> ScaledStroke = new List<Point>();
+            List<Point> scaledStroke = new List<Point>();
 
             // Get total width and height of gesture
-            double fGestureOffsetLeft = Input.Min(i => i.X);
-            double fGestureOffsetTop = Input.Min(i => i.Y);
-            double fGestureWidth = Input.Max(i => i.X) - fGestureOffsetLeft;
-            double fGestureHeight = Input.Max(i => i.Y) - fGestureOffsetTop;
+            double fGestureOffsetLeft = input.Min(i => i.X);
+            double fGestureOffsetTop = input.Min(i => i.Y);
+            double fGestureWidth = input.Max(i => i.X) - fGestureOffsetLeft;
+            double fGestureHeight = input.Max(i => i.Y) - fGestureOffsetTop;
 
             // Get each scale ratio
-            double dScaleX = Width / fGestureWidth;
-            double dScaleY = Height / fGestureHeight;
+            double dScaleX = width / fGestureWidth;
+            double dScaleY = height / fGestureHeight;
 
             // Scale on the longest axis
             if (fGestureWidth >= fGestureHeight)
             {
                 // Scale on X axis
                 // Clear current scaled stroke
-                ScaledStroke.Clear();
+                scaledStroke.Clear();
 
-                foreach (System.Drawing.Point currentPoint in Input)
-                    ScaledStroke.Add(new Point(((currentPoint.X - fGestureOffsetLeft) * dScaleX), ((currentPoint.Y - fGestureOffsetTop) * dScaleX)));
+                scaledStroke.AddRange(input.Select(currentPoint => new Point(((currentPoint.X - fGestureOffsetLeft) * dScaleX), ((currentPoint.Y - fGestureOffsetTop) * dScaleX))));
 
                 // Calculate new gesture width and height
-                _ScaledSize = new Size(Math.Floor(fGestureWidth * dScaleX), Math.Floor(fGestureHeight * dScaleX));
+                scaledSize = new Size(Math.Floor(fGestureWidth * dScaleX), Math.Floor(fGestureHeight * dScaleX));
             }
             else
             {
                 // Scale on X axis
                 // Clear current scaled stroke
-                ScaledStroke.Clear();
+                scaledStroke.Clear();
 
-                foreach (System.Drawing.Point currentPoint in Input)
-                    ScaledStroke.Add(new Point((currentPoint.X - fGestureOffsetLeft) * dScaleY, (currentPoint.Y - fGestureOffsetTop) * dScaleY));
+                scaledStroke.AddRange(input.Select(currentPoint => new Point((currentPoint.X - fGestureOffsetLeft) * dScaleY, (currentPoint.Y - fGestureOffsetTop) * dScaleY)));
 
                 // Calculate new gesture width and height
-                _ScaledSize = new Size(fGestureWidth * dScaleY, fGestureHeight * dScaleY);
+                scaledSize = new Size(fGestureWidth * dScaleY, fGestureHeight * dScaleY);
             }
 
-            return ScaledStroke.ToArray();
+            return scaledStroke.ToArray();
         }
 
         public static DrawingImage CreateImage(List<List<System.Drawing.Point>> points, Size size, Brush color)
@@ -68,45 +60,44 @@ namespace GestureSign.UI.Common
             if (points == null)
                 throw new Exception("You must provide a gesture before trying to generate a thumbnail");
             //  System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
-            Pen _DrawingPen = new Pen(color, 4) { StartLineCap = PenLineCap.Round, EndLineCap = PenLineCap.Round };
+            Pen drawingPen = new Pen(color, 4) { StartLineCap = PenLineCap.Round, EndLineCap = PenLineCap.Round };
 
-            Size _ScaledSize;
             PathGeometry pathGeometry = new PathGeometry();
 
             for (int i = 0; i < points.Count; i++)
             {
                 if (points[i].Count == 1)
                 {
-                    Geometry ellipse = new EllipseGeometry(new Point(size.Width * i + size.Width / 2, size.Height / 2), _DrawingPen.Thickness / 2, _DrawingPen.Thickness / 2);
+                    Geometry ellipse = new EllipseGeometry(new Point(size.Width * i + size.Width / 2, size.Height / 2), drawingPen.Thickness / 2, drawingPen.Thickness / 2);
                     pathGeometry.AddGeometry(ellipse);
                     continue;
                 }
-                StreamGeometry sg = new StreamGeometry();
-                sg.FillRule = FillRule.EvenOdd;
+                StreamGeometry sg = new StreamGeometry { FillRule = FillRule.EvenOdd };
                 using (StreamGeometryContext sgc = sg.Open())
                 {
                     // Create new size object accounting for pen width
-                    Size szeAdjusted = new Size(size.Width - _DrawingPen.Thickness - 1, (size.Height - _DrawingPen.Thickness - 1));
+                    Size szeAdjusted = new Size(size.Width - drawingPen.Thickness - 1, (size.Height - drawingPen.Thickness - 1));
 
-                    Point[] _ScaledPoints = ScaleGesture(points[i], szeAdjusted.Width - 10, szeAdjusted.Height - 10, out _ScaledSize);
+                    Size scaledSize;
+                    Point[] scaledPoints = ScaleGesture(points[i], szeAdjusted.Width - 10, szeAdjusted.Height - 10, out scaledSize);
 
                     // Define size that will mark the offset to center the gesture
-                    double iLeftOffset = (size.Width / 2) - (_ScaledSize.Width / 2);
-                    double iTopOffset = (size.Height / 2) - (_ScaledSize.Height / 2);
+                    double iLeftOffset = (size.Width / 2) - (scaledSize.Width / 2);
+                    double iTopOffset = (size.Height / 2) - (scaledSize.Height / 2);
                     Vector sizOffset = new Vector(iLeftOffset + i * size.Width, iTopOffset);
-                    sgc.BeginFigure(Point.Add(_ScaledPoints[0], sizOffset), false, false);
-                    for (int j = 0; j < _ScaledPoints.Length; j++)
+                    sgc.BeginFigure(Point.Add(scaledPoints[0], sizOffset), false, false);
+                    foreach (Point p in scaledPoints)
                     {
-                        sgc.LineTo(Point.Add(_ScaledPoints[j], sizOffset), true, true);
+                        sgc.LineTo(Point.Add(p, sizOffset), true, true);
                     }
-                    DrawArrow(sgc, _ScaledPoints, sizOffset, _DrawingPen.Thickness);
+                    DrawArrow(sgc, scaledPoints, sizOffset, drawingPen.Thickness);
                 }
                 sg.Freeze();
                 pathGeometry.AddGeometry(sg);
             }
             //  myPath.Data = sg;
             pathGeometry.Freeze();
-            GeometryDrawing drawing = new GeometryDrawing(null, _DrawingPen, pathGeometry);
+            GeometryDrawing drawing = new GeometryDrawing(null, drawingPen, pathGeometry);
             drawing.Freeze();
             DrawingImage drawingImage = new DrawingImage(drawing);
             drawingImage.Freeze();
@@ -116,8 +107,8 @@ namespace GestureSign.UI.Common
         }
         private static void DrawArrow(StreamGeometryContext streamGeometryContext, Point[] points, Vector sizeOffset, double thickness)
         {
-            double HeadWidth = thickness;
-            double HeadHeight = thickness * 0.8;
+            double headWidth = thickness;
+            double headHeight = thickness * 0.8;
 
             Point pt1 = Point.Add(points[points.Length - 2], sizeOffset);
             Point pt2 = Point.Add(points[points.Length - 1], sizeOffset);
@@ -128,12 +119,12 @@ namespace GestureSign.UI.Common
 
 
             Point pt3 = new Point(
-                pt2.X + (HeadWidth * cost - HeadHeight * sint),
-                pt2.Y + (HeadWidth * sint + HeadHeight * cost));
+                pt2.X + (headWidth * cost - headHeight * sint),
+                pt2.Y + (headWidth * sint + headHeight * cost));
 
             Point pt4 = new Point(
-                pt2.X + (HeadWidth * cost + HeadHeight * sint),
-                pt2.Y - (HeadHeight * cost - HeadWidth * sint));
+                pt2.X + (headWidth * cost + headHeight * sint),
+                pt2.Y - (headHeight * cost - headWidth * sint));
 
             streamGeometryContext.BeginFigure(pt1, true, false);
             streamGeometryContext.LineTo(pt2, true, true);
