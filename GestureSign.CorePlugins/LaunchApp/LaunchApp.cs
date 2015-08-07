9808ba987a0b26cfe18fd87a3121dcce860a6a5c
TransposonY <ccfyzh@gmail.com>
2015-08-07 12:23:30 +0800

    Add Launch Store App Plugin

---
+++ GestureSign.CorePlugins/LaunchApp/LaunchApp.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using GestureSign.Common.Localization;
+using GestureSign.Common.Plugins;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace GestureSign.CorePlugins.LaunchApp
+{
+    public class LaunchApp : IPlugin
+    {
+        #region Private Variables 
+
+        private LaunchAppView _gui;
+        private KeyValuePair<string, string> _appInfo;
+
+        #endregion
+
+        //[DllImport("shell32.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        //private static extern void SHCreateItemFromParsingName(
+        //          [In] [MarshalAs(UnmanagedType.LPWStr)] string pszPath,
+        //          [In] IntPtr pbc,
+        //          [In] [MarshalAs(UnmanagedType.LPStruct)] Guid iIdIShellItem,
+        //          [Out] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)] out IShellItem iShellItem);
+
+        //[DllImport("shell32.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        //private static extern void SHCreateShellItemArrayFromShellItem(
+        //    [In] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)] IShellItem psi,
+        //    [In] [MarshalAs(UnmanagedType.LPStruct)] Guid iIdIShellItem,
+        //    [Out] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)] out IShellItemArray iShellItemArray);
+
+        #region PInvoke Declarations
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// key:AppUserModelId
+        /// value:ApplicationName
+        /// </summary>
+        public KeyValuePair<string, string> AppInfo
+        {
+            get { return _appInfo; }
+            set { _appInfo = value; }
+        }
+
+        public string Name => LocalizationProvider.Instance.GetTextValue("CorePlugins.LaunchApp.Name");
+
+        public string Description => AppInfo.Value == null ? LocalizationProvider.Instance.GetTextValue("CorePlugins.LaunchApp.Name") :
+            string.Format(LocalizationProvider.Instance.GetTextValue("CorePlugins.LaunchApp.Description"), AppInfo.Value);
+
+        public UserControl GUI => _gui ?? (_gui = CreateGui());
+
+        public string Category => LocalizationProvider.Instance.GetTextValue("CorePlugins.LaunchApp.Category");
+
+        public bool IsAction => true;
+
+        #endregion
+
+        #region Public Methods
+
+        public void Initialize()
+        {
+
+        }
+
+        public bool Gestured(PointInfo actionPoint)
+        {
+            if (AppInfo.Key == null) return false;
+            ApplicationActivationManager appActiveManager = new ApplicationActivationManager();
+            uint pid;
+            appActiveManager.ActivateApplication(AppInfo.Key, null, ActivateOptions.None, out pid);
+
+            //IShellItemArray array = GetShellItemArray(@"C:\temp\somefile.xyz");
+            //appActiveManager.ActivateForFile("2c123c17-8b21-4eb8-8b7f-fdc35c8b7718_n2533ggrncqjt!App", array, "Open",
+            //    out pid);
+
+
+            //Process explorer = new Process();
+            //explorer.StartInfo.FileName = "explorer.exe";
+            //explorer.StartInfo.Arguments = @"shell:AppsFolder\" + AppInfo.Key;
+            //explorer.Start();
+
+            return true;
+        }
+
+        public bool Deserialize(string serializedData)
+        {
+            return PluginHelper.DeserializeSettings(serializedData, out _appInfo);
+        }
+
+        public string Serialize()
+        {
+            if (_gui != null)
+                _appInfo = _gui.SelectedAppInfo;
+
+            return PluginHelper.SerializeSettings(AppInfo);
+        }
+
+        #endregion
+
+        #region ApplicationActivationManager
+
+        //http://stackoverflow.com/q/12925748
+
+        public enum ActivateOptions
+        {
+            None = 0x00000000,  // No flags set
+            DesignMode = 0x00000001,  // The application is being activated for design mode, and thus will not be able to
+                                      // to create an immersive window. Window creation must be done by design tools which
+                                      // load the necessary components by communicating with a designer-specified service on
+                                      // the site chain established on the activation manager.  The splash screen normally
+                                      // shown when an application is activated will also not appear.  Most activations
+                                      // will not use this flag.
+            NoErrorUI = 0x00000002,  // Do not show an error dialog if the app fails to activate.                                
+            NoSplashScreen = 0x00000004,  // Do not show the splash screen when activating the app.
+        }
+
+        [ComImport, Guid("2e941141-7f97-4756-ba1d-9decde894a3d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        interface IApplicationActivationManager
+        {
+            // Activates the specified immersive application for the "Launch" contract, passing the provided arguments
+            // string into the application.  Callers can obtain the process Id of the application instance fulfilling this contract.
+            IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
+
+            IntPtr ActivateForFile([In] String appUserModelId,
+                [In] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)] /*IShellItemArray* */ IShellItemArray
+                    itemArray, [In] String verb, [Out] out UInt32 processId);
+            IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
+        }
+
+        [ComImport, Guid("45BA127D-10A8-46EA-8AB7-56EA9078943C")]//Application Activation Manager
+        class ApplicationActivationManager : IApplicationActivationManager
+        {
+            [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)/*, PreserveSig*/]
+            public extern IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
+
+            [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+            public extern IntPtr ActivateForFile([In] String appUserModelId,
+                [In] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)] /*IShellItemArray* */ IShellItemArray
+                    itemArray, [In] String verb, [Out] out UInt32 processId);
+            [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+            public extern IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
+        interface IShellItem
+        {
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("b63ea76d-1f85-456f-a19c-48159efa858b")]
+        interface IShellItemArray
+        {
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private LaunchAppView CreateGui()
+        {
+            LaunchAppView newGui = new LaunchAppView() { DataContext = this };
+            return newGui;
+        }
+        //private static IShellItemArray GetShellItemArray(string sourceFile)
+        //{
+        //    IShellItem item = GetShellItem(sourceFile);
+        //    IShellItemArray array = GetShellItemArray(item);
+
+        //    return array;
+        //}
+
+        //private static IShellItem GetShellItem(string sourceFile)
+        //{
+        //    IShellItem iShellItem = null;
+        //    Guid iIdIShellItem = new Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe");
+        //    SHCreateItemFromParsingName(sourceFile, IntPtr.Zero, iIdIShellItem, out iShellItem);
+
+        //    return iShellItem;
+        //}
+
+        //private static IShellItemArray GetShellItemArray(IShellItem shellItem)
+        //{
+        //    IShellItemArray iShellItemArray = null;
+        //    Guid iIdIShellItemArray = new Guid("b63ea76d-1f85-456f-a19c-48159efa858b");
+        //    SHCreateShellItemArrayFromShellItem(shellItem, iIdIShellItemArray, out iShellItemArray);
+
+        //    return iShellItemArray;
+        //}
+
+
+        #endregion
+
+        #region Host Control
+
+        public IHostControl HostControl { get; set; }
+
+        #endregion
+    }
+}
---
+++ GestureSign.CorePlugins/LaunchApp/LaunchAppView.xaml.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Xml;
+using Windows.ApplicationModel;
+using Windows.Management.Deployment;
+using Microsoft.Win32;
+
+namespace GestureSign.CorePlugins.LaunchApp
+{
+    /// <summary>
+    ///     Interaction logic for LaunchAppView.xaml
+    /// </summary>
+    public partial class LaunchAppView : UserControl
+    {
+        public LaunchAppView()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("shlwapi.dll", BestFitMapping = false, CharSet = CharSet.Unicode, ExactSpelling = true,
+            SetLastError = false, ThrowOnUnmappableChar = true)]
+        private static extern int SHLoadIndirectString(string pszSource, StringBuilder pszOutBuf, int cchOutBuf,
+            IntPtr ppvReserved);
+
+        public KeyValuePair<string, string> SelectedAppInfo
+        {
+            get
+            {
+                Model model = comboBox.SelectedItem as Model;
+                return model?.AppInfo ?? new KeyValuePair<string, string>();
+            }
+        }
+
+        private bool IsWindows10()
+        {
+            try
+            {
+                var rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+                return rk != null && rk.GetValue("ProductName").ToString().Contains("10");
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void comboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            var apps = new List<Model>(10);
+
+            var currentUser = WindowsIdentity.GetCurrent();
+            var sid = currentUser.User.ToString();
+
+            var packageManager = new PackageManager();
+            var packages = packageManager.FindPackagesForUser(sid);
+
+
+            var isWin10 = IsWindows10();
+            if (isWin10)
+            {
+                var appXInfos = GetAppXInfosFromReg();
+                if (appXInfos == null || appXInfos.Count == 0) return;
+
+                foreach (var package in packages)
+                {
+                    if (package.IsFramework) continue;
+                    if (appXInfos.ContainsKey(package.Id.FamilyName))
+                    {
+                        var appXInfo = appXInfos[package.Id.FamilyName];
+                        string backgroundColor;
+                        var displayName = ExtractDisplayName(package, appXInfo.ApplicationName);
+                        var logo = ExtractDisplayIconFromReg(package.InstalledLocation.Path, appXInfo.ApplicationIcon);
+                        var model = new Model
+                        {
+                            Logo = logo,
+                            AppInfo = new KeyValuePair<string, string>(appXInfo.AppUserModelId, displayName)
+                        };
+                        if (!ReadAppxInfoFromXml(package.InstalledLocation.Path, out displayName, out logo,
+                            out backgroundColor))
+                            continue;
+                        var convertFromString = ColorConverter.ConvertFromString(backgroundColor);
+                        if (convertFromString != null)
+                            model.BackgroundColor = new SolidColorBrush((Color)convertFromString);
+                        apps.Add(model);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var package in packages)
+                {
+                    if (package.IsFramework) continue;
+
+                    string displayName, logo, backgroundColor;
+                    if (
+                        !ReadAppxInfoFromXml(package.InstalledLocation.Path, out displayName, out logo,
+                            out backgroundColor))
+                        continue;
+
+                    displayName = ExtractDisplayName(package.InstalledLocation.Path, package.Id.Name, displayName);
+                    logo = ExtractDisplayIcon(package.InstalledLocation.Path, logo);
+
+                    var convertFromString = ColorConverter.ConvertFromString(backgroundColor);
+                    if (convertFromString == null) continue;
+                    var model = new Model
+                    {
+                        AppInfo = new KeyValuePair<string, string>(GetAppUserModelId(package.Id.FullName), displayName),
+                        BackgroundColor = new SolidColorBrush((Color)convertFromString),
+                        Logo = logo
+                    };
+                    apps.Add(model);
+                }
+            }
+
+            comboBox.ItemsSource = apps.OrderBy(app => app.AppInfo.Value).ToList();
+
+            var launchApp = DataContext as LaunchApp;
+            if (launchApp != null)
+            {
+                comboBox.SelectedItem = apps.Find(m => m.AppInfo.Equals(launchApp.AppInfo));
+            }
+        }
+
+        private string ExtractDisplayName(Package package, string displayName)
+        {
+            var priPath = Path.Combine(package.InstalledLocation.Path, "resources.pri");
+
+            var manifestString = displayName.Replace(package.Id.FullName, priPath);
+            var outBuff = new StringBuilder(128);
+            SHLoadIndirectString(manifestString, outBuff, outBuff.Capacity, IntPtr.Zero);
+            return outBuff.ToString();
+        }
+
+        private string ExtractDisplayName(string path, string packageName, string displayName)
+        {
+            var priPath = Path.Combine(path, "resources.pri");
+            Uri uri;
+            if (!Uri.TryCreate(displayName, UriKind.Absolute, out uri))
+                return displayName;
+
+            var resource = $"ms-resource://{packageName}/resources/{uri.Segments.Last()}";
+            var name = ExtractStringFromPriFile(priPath, resource);
+            if (!string.IsNullOrWhiteSpace(name))
+                return name;
+
+            var res = string.Concat(uri.Segments.Skip(1));
+            resource = $"ms-resource://{packageName}/{res}";
+            return ExtractStringFromPriFile(priPath, resource);
+        }
+
+        private string ExtractStringFromPriFile(string pathToPri, string resourceKey)
+        {
+            string sWin8ManifestString = $"@{{{pathToPri}? {resourceKey}}}";
+            var outBuff = new StringBuilder(128);
+            SHLoadIndirectString(sWin8ManifestString, outBuff, outBuff.Capacity, IntPtr.Zero);
+            return outBuff.ToString();
+        }
+
+        private string ExtractDisplayIcon(string dir, string logo)
+        {
+            var logoPath = Path.Combine(dir, logo.TrimStart('\\'));
+            if (File.Exists(logoPath))
+                return logoPath;
+
+            logoPath = Path.Combine(dir, Path.ChangeExtension(logoPath, "scale-100.png"));
+            if (File.Exists(logoPath))
+                return logoPath;
+
+            var localized = Path.Combine(dir, "en-us", logo); //TODO: How determine if culture parameter is necessary?
+            localized = Path.Combine(dir, Path.ChangeExtension(localized, "scale-100.png"));
+            return localized;
+        }
+
+        private string ExtractDisplayIconFromReg(string dir, string logo)
+        {
+            logo = logo.TrimEnd('}').Split('?')[1];
+
+            Uri uri;
+            if (!Uri.TryCreate(logo, UriKind.Absolute, out uri))
+                return string.Empty;
+            //remove "/Files"
+            logo = uri.AbsolutePath.Substring(6).Replace('/', '\\');
+
+            return ExtractDisplayIcon(dir, logo);
+        }
+
+        private bool ReadAppxInfoFromXml(string path, out string displayName, out string logo,
+            out string backgroundColor)
+        {
+            var file = Path.Combine(path, "AppxManifest.xml");
+            backgroundColor = displayName = logo = null;
+            if (!File.Exists(file)) return false;
+            using (var xtr = new XmlTextReader(file) { WhitespaceHandling = WhitespaceHandling.None })
+            {
+                while (xtr.Read())
+                {
+                    if (xtr.NodeType != XmlNodeType.Element) continue;
+
+                    if ("DisplayName".Equals(xtr.Name, StringComparison.OrdinalIgnoreCase))
+                    {
+                        xtr.Read();
+                        displayName = xtr.Value;
+                    }
+                    else if ("Logo".Equals(xtr.Name, StringComparison.OrdinalIgnoreCase))
+                    {
+                        xtr.Read();
+                        logo = xtr.Value;
+                    }
+                    else if ("VisualElements".Equals(xtr.LocalName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        backgroundColor = xtr.GetAttribute("BackgroundColor");
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     Windows 10 only
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<string, AppXInfo> GetAppXInfosFromReg()
+        {
+            var dic = new Dictionary<string, AppXInfo>(10);
+
+            using (var key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Classes\"))
+            {
+                if (key == null) return null;
+                var appKeys = from k in key.GetSubKeyNames()
+                              where k.StartsWith("AppX")
+                              select k;
+                foreach (var appKey in appKeys)
+                {
+                    using (var appRegKey = key.OpenSubKey(appKey))
+                    {
+                        if (appRegKey == null) continue;
+                        using (var applicationKey = appRegKey.OpenSubKey("Application"))
+                        {
+                            if (applicationKey == null) continue;
+                            var appUserModelId = applicationKey.GetValue("AppUserModelId").ToString();
+                            var applicationIcon = applicationKey.GetValue("ApplicationIcon").ToString();
+                            var applicationName = applicationKey.GetValue("ApplicationName").ToString();
+                            var familyName = GetFamilyName(appUserModelId);
+
+                            if (familyName != null && !dic.ContainsKey(familyName))
+                                dic.Add(familyName, new AppXInfo
+                                {
+                                    ApplicationIcon = applicationIcon,
+                                    ApplicationName = applicationName,
+                                    AppUserModelId = appUserModelId
+                                });
+                            applicationKey.Close();
+                        }
+                    }
+                }
+            }
+            return dic;
+        }
+
+        private string GetAppUserModelId(string packageFullName)
+        {
+            var str = string.Empty;
+            using (var key = Registry.CurrentUser.CreateSubKey(
+                $@"SOFTWARE\Classes\ActivatableClasses\Package\{packageFullName}\Server\"))
+            {
+                if (key == null) return str;
+
+                var appKeys = from k in key.GetSubKeyNames()
+                              where k.StartsWith("Appex")
+                              select k;
+
+                foreach (var appKey in appKeys)
+                {
+                    using (var serverKey = key.OpenSubKey(appKey))
+                    {
+                        if (serverKey?.GetValue("AppUserModelId") == null) continue;
+                        str = serverKey.GetValue("AppUserModelId").ToString();
+                        serverKey.Close();
+                        break;
+                    }
+                }
+            }
+
+            return str;
+        }
+
+        private string GetFamilyName(string appUserModelId)
+        {
+            if (appUserModelId == null) return null;
+            var str = appUserModelId.Split('!');
+            return str.Length != 0 ? str[0] : null;
+        }
+
+        private class AppXInfo
+        {
+            public string ApplicationIcon { get; set; }
+            public string ApplicationName { get; set; }
+            public string AppUserModelId { get; set; }
+        }
+
+        private class Model
+        {
+            public string Logo { get; set; }
+            public KeyValuePair<string, string> AppInfo { get; set; }
+            public Brush BackgroundColor { get; set; }
+        }
+    }
+}
