a945b5576a8525f9bbff219dc74412cab9e603c5
TransposonY <ccfyzh@gmail.com>
2015-08-19 21:43:01 +0800

    version 0.14.2

--- GestureSign.ControlPanel/Properties/AssemblyInfo.cs
+++ GestureSign.ControlPanel/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.14.*")]
-[assembly: AssemblyFileVersion("0.14.1.0")]
+[assembly: AssemblyFileVersion("0.14.2.0")]
