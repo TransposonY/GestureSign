b702e5617c87d873a5c1b505f863dd28ef3bec12
TransposonY <ccfyzh@gmail.com>
2015-09-06 10:30:03 +0800

    Update translation.

--- GestureSign.Common/Applications/GlobalApplication.cs
+++ GestureSign.Common/Applications/GlobalApplication.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Runtime.Serialization;
+﻿using GestureSign.Common.Localization;
 
 namespace GestureSign.Common.Applications
 {
-	public class GlobalApplication : ApplicationBase
-	{
-		#region IApplication Properties
+    public class GlobalApplication : ApplicationBase
+    {
+        #region IApplication Properties
 
-		public override string Name
-		{
-			get { return "所有程序"; }
-			//set { /* Set only exists for deserialization purposes */ }
-		}
+        public override string Name
+        {
+            get { return LocalizationProvider.Instance.GetTextValue("Common.AllApplications"); ; }
+            //set { /* Set only exists for deserialization purposes */ }
+        }
 
-		public override MatchUsing MatchUsing
-		{
-			get { return MatchUsing.All; }
-		//	set { /* Set only exists for deserialization purposes */ }
-		}
+        public override MatchUsing MatchUsing
+        {
+            get { return MatchUsing.All; }
+            //	set { /* Set only exists for deserialization purposes */ }
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
