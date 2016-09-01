a2ae39c6d7e48b0c3c586417870e38fc08c67e3a
TransposonY <ccfyzh@gmail.com>
2016-09-01 23:17:08 +0800

    version 3.1

--- GestureSign.Common/Properties/AssemblyInfo.cs
+++ GestureSign.Common/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using GestureSign.Common;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
