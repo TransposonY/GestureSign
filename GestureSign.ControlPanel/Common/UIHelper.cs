a09991d701ea3761d8a8da47a2e9f903042865ca
TransposonY <ccfyzh@gmail.com>
2016-10-01 11:55:01 +0800

    MahApps.Metro.1.3.0

--- GestureSign.ControlPanel/Common/UIHelper.cs
+++ GestureSign.ControlPanel/Common/UIHelper.cs
@@ -15,52 +15,58 @@ namespace GestureSign.ControlPanel.Common
 
         public static ResourceDictionary GetSystemAccent()
         {
-            SolidColorBrush accentColorBrush = (SolidColorBrush)SystemParameters.WindowGlassBrush;
-            Double dY = accentColorBrush.Color.R * 0.299 + accentColorBrush.Color.G * 0.587 + accentColorBrush.Color.B * 0.114;
+            SolidColorBrush AccentBaseColorBrush = (SolidColorBrush)SystemParameters.WindowGlassBrush;
+            Double dY = AccentBaseColorBrush.Color.R * 0.299 + AccentBaseColorBrush.Color.G * 0.587 + AccentBaseColorBrush.Color.B * 0.114;
             if (dY >= 192 || dY < 50)
             {
                 return null;
             }
             ResourceDictionary rd = new ResourceDictionary();
 
-            Color AccentColor = accentColorBrush.Color;
+            Color AccentBaseColor = AccentBaseColorBrush.Color;
 
-            Color HighlightColor = Color.Multiply(AccentColor, 0.73f);
+            Color HighlightColor = Color.Multiply(AccentBaseColor, 0.73f);
             HighlightColor.A = 0xFF;
             SolidColorBrush HighlightBrush = new SolidColorBrush(HighlightColor);
 
-            Color AccentColor2 = AccentColor;
-            AccentColor2.A = 0xCC;
+            Color AccentColor = AccentBaseColor;
+            AccentColor.A = 0xCC;
+            SolidColorBrush AccentColorBrush = new SolidColorBrush(AccentColor);
+
+            Color AccentColor2 = AccentBaseColor;
+            AccentColor2.A = 0x99;
             SolidColorBrush AccentColorBrush2 = new SolidColorBrush(AccentColor2);
 
-            Color AccentColor3 = AccentColor;
-            AccentColor3.A = 0x99;
+            Color AccentColor3 = AccentBaseColor;
+            AccentColor3.A = 0x66;
             SolidColorBrush AccentColorBrush3 = new SolidColorBrush(AccentColor3);
 
-            Color AccentColor4 = AccentColor;
-            AccentColor4.A = 0x66;
+            Color AccentColor4 = AccentBaseColor;
+            AccentColor4.A = 0x33;
             SolidColorBrush AccentColorBrush4 = new SolidColorBrush(AccentColor4);
             //Color AccentColor= HighlightColor,
             rd.Add("HighlightColor", HighlightColor);
+            rd.Add("AccentBaseColor", AccentBaseColor);
             rd.Add("AccentColor", AccentColor);
             rd.Add("AccentColor2", AccentColor2);
             rd.Add("AccentColor3", AccentColor3);
             rd.Add("AccentColor4", AccentColor4);
 
             rd.Add("HighlightBrush", HighlightBrush);
-            rd.Add("AccentColorBrush", accentColorBrush);
+            rd.Add("AccentBaseColorBrush", AccentBaseColorBrush);
+            rd.Add("AccentColorBrush", AccentColorBrush);
             rd.Add("AccentColorBrush2", AccentColorBrush2);
             rd.Add("AccentColorBrush3", AccentColorBrush3);
             rd.Add("AccentColorBrush4", AccentColorBrush4);
 
-            rd.Add("WindowTitleColorBrush", accentColorBrush);
+            rd.Add("WindowTitleColorBrush", AccentColorBrush);
             rd.Add("AccentSelectedColorBrush", Brushes.White);
 
             rd.Add("IdealForegroundColor", Colors.White);
             rd.Add("IdealForegroundColorBrush", Brushes.White);
 
-            rd.Add("CheckmarkFill", accentColorBrush);
-            rd.Add("RightArrowFill", accentColorBrush);
+            rd.Add("CheckmarkFill", AccentColorBrush);
+            rd.Add("RightArrowFill", AccentColorBrush);
             return rd;
         }
 
