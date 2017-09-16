60ca948be5ed71d380a44f5b197253a3af05ce7f
TransposonY <ccfyzh@gmail.com>
2017-09-16 23:36:16 +0800

    update action title

--- GestureSign.ControlPanel/Converters/ActionTitleConverter.cs
+++ GestureSign.ControlPanel/Converters/ActionTitleConverter.cs
@@ -2,7 +2,6 @@ using GestureSign.Common.Applications;
 using GestureSign.Common.Configuration;
 using GestureSign.Common.Localization;
 using MahApps.Metro.Controls;
-using ManagedWinapi.Hooks;
 using System;
 using System.Globalization;
 using System.Windows.Data;
@@ -17,11 +16,24 @@ namespace GestureSign.ControlPanel.Converters
         {
             var action = (IAction)value;
             if (action == null) return null;
+
             var actionName = string.IsNullOrWhiteSpace(action.Name) ? LocalizationProvider.Instance.GetTextValue("Action.NewAction") : action.Name;
-            var hotKeyString = action.Hotkey != null ? "(" + new HotKey(KeyInterop.KeyFromVirtualKey(action.Hotkey.KeyCode), (ModifierKeys)action.Hotkey.ModifierKeys).ToString() + ")" : string.Empty;
-            var mouseString = action.MouseHotkey == MouseActions.None ?
-                string.Empty : "(" + ViewModel.MouseActionDescription.DescriptionDict[AppConfig.DrawingButton] + " + " + ViewModel.MouseActionDescription.DescriptionDict[action.MouseHotkey] + ")";
-            return $"{actionName} {hotKeyString} {mouseString}";
+
+            string triggers = null;
+            if (action.ContinuousGesture != null)
+            {
+                triggers += string.Format(LocalizationProvider.Instance.GetTextValue("Action.Fingers"), action.ContinuousGesture.ContactCount) + " " + LocalizationProvider.Instance.GetTextValue("Action." + action.ContinuousGesture.Gesture) + " / ";
+            }
+            if (action.Hotkey != null)
+            {
+                triggers += new HotKey(KeyInterop.KeyFromVirtualKey(action.Hotkey.KeyCode), (ModifierKeys)action.Hotkey.ModifierKeys).ToString() + " / ";
+            }
+            if ((action.MouseHotkey & AppConfig.DrawingButton) != 0)
+            {
+                triggers += ViewModel.MouseActionDescription.DescriptionDict[AppConfig.DrawingButton] + " + " + ViewModel.MouseActionDescription.DescriptionDict[action.MouseHotkey] + " / ";
+            }
+
+            return string.IsNullOrEmpty(triggers) ? actionName : $"{actionName} ({triggers.TrimEnd(' ', '/')})";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
