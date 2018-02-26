475f6c1eadc7a765f5d985ce0b78ca574bc52718
TransposonY <ccfyzh@gmail.com>
2018-02-26 23:54:22 +0800

    update TouchKeyboard plugin for windows 10 1709

--- GestureSign.CorePlugins/TouchKeyboard/TouchKeyboard.cs
+++ GestureSign.CorePlugins/TouchKeyboard/TouchKeyboard.cs
@@ -32,10 +32,19 @@ namespace GestureSign.CorePlugins.TouchKeyboard
         private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
         [DllImport("user32.dll", EntryPoint = "FindWindowEx", SetLastError = true)]
-        private static extern IntPtr FindWindowEx(IntPtr hwndParent, uint hwndChildAfter, string lpszClass, string lpszWindow);
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
 
         [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Auto, EntryPoint = "PostMessage")]
         private static extern bool PostMessage(IntPtr hWnd, int Msg, uint wParam, uint lParam);
+
+        [ComImport]
+        [Guid("37c994e7-432b-4834-a2f7-dce1f13b834b")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        interface ITipInvocation
+        {
+            void Toggle(IntPtr hwnd);
+        }
+
         #endregion
 
         #region Public Properties
@@ -126,35 +135,37 @@ namespace GestureSign.CorePlugins.TouchKeyboard
                     // older windows versions
                     path = Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\osk.exe";
                 }
-                Process Process = new Process();
-                // Expand environment variable to support %SYSTEMROOT%, etc.
-                Process.StartInfo.FileName = path;
-                Process.Start();
+                using (Process Process = new Process())
+                {
+                    Process.StartInfo.FileName = path;
+                    Process.Start();
+                }
                 return true;
             }
             catch { return false; }
         }
 
-        private bool ShowKeyboard()
+        private bool ToggleVisibilityByTipBand()
         {
             //find taskbar 
             IntPtr hwndTaskbar = FindWindow("Shell_TrayWnd", null);
             //Win 10
-            IntPtr hwndTrayNotifyWnd = FindWindowEx(hwndTaskbar, 0, "TrayNotifyWnd", null);
-            IntPtr hwndTIPBand = FindWindowEx(hwndTrayNotifyWnd, 0, "TIPBand", null);
+            IntPtr hwndTrayNotifyWnd = FindWindowEx(hwndTaskbar, IntPtr.Zero, "TrayNotifyWnd", null);
+            IntPtr hwndTIPBand = FindWindowEx(hwndTrayNotifyWnd, IntPtr.Zero, "TIPBand", null);
             if (hwndTIPBand == IntPtr.Zero)
             {
                 //Win 8
-                IntPtr hwndReBar = FindWindowEx(hwndTaskbar, 0, "ReBarWindow32", null);
-                hwndTIPBand = FindWindowEx(hwndReBar, 0, "TIPBand", null);
-                if (hwndTIPBand == IntPtr.Zero) return StartProcess();
+                IntPtr hwndReBar = FindWindowEx(hwndTaskbar, IntPtr.Zero, "ReBarWindow32", null);
+                hwndTIPBand = FindWindowEx(hwndReBar, IntPtr.Zero, "TIPBand", null);
+                if (hwndTIPBand == IntPtr.Zero)
+                    return false;
             }
             else
             {
                 SystemWindow ww = new SystemWindow(hwndTIPBand);
                 if (ww.Size.Height == 0 || ww.Size.Width == 0)
                 {
-                    return StartProcess();
+                    return false;
                 }
             }
             SendMessage(hwndTIPBand, WM_LBUTTONDOWN, (IntPtr)1, 0x160010);
@@ -163,13 +174,109 @@ namespace GestureSign.CorePlugins.TouchKeyboard
             return true;
         }
 
+        private bool ShowKeyboard()
+        {
+            if (!IsKeyboardOpen())
+            {
+                if (!ToggleVisibilityByTipBand())
+                {
+                    var processes = Process.GetProcessesByName("TabTip");
+                    if (processes.Length != 0)
+                    {
+                        foreach (var p in processes)
+                        {
+                            p.Dispose();
+                        }
+                        try
+                        {
+                            ToggleVisibility();
+                            return true;
+                        }
+                        catch { }
+                    }
+                    else
+                    {
+                        if (StartProcess())
+                        {
+                            for (int i = 0; i < 6; i++)
+                            {
+                                System.Threading.Thread.Sleep(100);
+                                if (IsKeyboardOpen())
+                                    return true;
+                            }
+                            ToggleVisibility();
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
         private bool HideKeyboard()
         {
-            var keyboardHwnd = FindWindow("IPTip_Main_Window", null);
+            try
+            {
+                if (IsKeyboardOpen())
+                    if (!ToggleVisibilityByTipBand())
+                    {
+                        ToggleVisibility();
+                    }
+                return true;
+            }
+            catch
+            {
+                var keyboardHwnd = FindWindow("IPTip_Main_Window", null);
 
-            if (keyboardHwnd != IntPtr.Zero)
-                PostMessage(keyboardHwnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-            return true;
+                if (keyboardHwnd != IntPtr.Zero)
+                {
+                    PostMessage(keyboardHwnd, WM_SYSCOMMAND, SC_CLOSE, 0);
+                    return true;
+                }
+                else return false;
+            }
+        }
+
+        private bool IsKeyboardOpen()
+        {
+            // Reference https://stackoverflow.com/a/48545074
+            if (Environment.OSVersion.Version >= new Version(10, 0, 16299))
+            {
+                const string WindowParentClass1709 = "ApplicationFrameWindow";
+                const string WindowClass1709 = "Windows.UI.Core.CoreWindow";
+                const string WindowCaption1709 = "Microsoft Text Input Application";
+
+                // if there is a top-level window - the keyboard is closed
+                var wnd = FindWindowEx(IntPtr.Zero, IntPtr.Zero, WindowClass1709, WindowCaption1709);
+                if (wnd != IntPtr.Zero)
+                    return false;
+
+                var parent = IntPtr.Zero;
+                while (true)
+                {
+                    parent = FindWindowEx(IntPtr.Zero, parent, WindowParentClass1709, null);
+                    if (parent == IntPtr.Zero)
+                        break; // no more windows, keyboard state is unknown
+
+                    // if it's a child of a WindowParentClass1709 window - the keyboard is open
+                    wnd = FindWindowEx(parent, IntPtr.Zero, WindowClass1709, WindowCaption1709);
+                    if (wnd != IntPtr.Zero)
+                        return true;
+                }
+            }
+
+            var keyboardHwnd = FindWindow("IPTip_Main_Window", null);
+            if (keyboardHwnd == IntPtr.Zero)
+                return false;
+            var taptipWindow = new SystemWindow(keyboardHwnd);
+            return taptipWindow.Visible && taptipWindow.Enabled;
+        }
+
+        private void ToggleVisibility()
+        {
+            var type = Type.GetTypeFromCLSID(Guid.Parse("4ce576fa-83dc-4F88-951c-9d0782b4e376"));
+            var instance = (ITipInvocation)Activator.CreateInstance(type);
+            instance.Toggle(SystemWindow.DesktopWindow.HWnd);
+            Marshal.ReleaseComObject(instance);
         }
 
         #endregion
@@ -189,12 +296,7 @@ namespace GestureSign.CorePlugins.TouchKeyboard
             }
             else
             {
-                var keyboardHwnd = FindWindow("IPTip_Main_Window", null);
-                if (keyboardHwnd == IntPtr.Zero)
-                    return ShowKeyboard();
-
-                var taptipWindow = new SystemWindow(keyboardHwnd);
-                return taptipWindow.Visible && taptipWindow.Enabled ? HideKeyboard() : ShowKeyboard();
+                return IsKeyboardOpen() ? HideKeyboard() : ShowKeyboard();
             }
         }
 
