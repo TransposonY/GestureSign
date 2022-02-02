2475a39f43fd2cf716de9d33608c1e20a4dc1b41
TransposonY <ccfyzh@gmail.com>
2022-02-02 09:44:29 +0800

    remove SimulateDoubleTap in PointerInputTargetWindow

--- GestureSign.Daemon/Filtration/PointerInputTargetWindow.cs
+++ GestureSign.Daemon/Filtration/PointerInputTargetWindow.cs
@@ -12,16 +12,10 @@ namespace GestureSign.Daemon.Filtration
 {
     public class PointerInputTargetWindow : Form
     {
-        private readonly float _doubleTapRadius;
-        private readonly int _doubleTapTime;
-        private readonly List<string> _doubleTapList = new List<string> { "MMCMainFrame", "CabinetWClass", "ExploreWClass", "Shell_TrayWnd", "Progman", "WorkerW" };
         private bool _isRegistered;
         private int _blockTouchInputThreshold;
         private Dictionary<int, int> _pointerIdList = new Dictionary<int, int>(10);
         private Queue<int> _idPool = new Queue<int>(10);
-        private POINT? _doubleTapPoint;
-        private int _lastTapTime;
-        private bool _blockTap;
         private bool _isInitialized = false;
         private bool _tempDisable;
         private int _lastFrameID;
@@ -30,8 +24,6 @@ namespace GestureSign.Daemon.Filtration
         {
             CreateHandle();
             ResetIdPool();
-            _doubleTapRadius = 3 * SystemInformation.DoubleClickSize.Width * DpiHelper.GetSystemDpi() / 96f;
-            _doubleTapTime = SystemInformation.DoubleClickTime;
         }
 
         public int BlockTouchInputThreshold
@@ -146,8 +138,6 @@ namespace GestureSign.Daemon.Filtration
 
             if (pointerInfos.Length != ptis.Count) return;
 
-            if (SimulateDoubleTap(ptis) || _blockTap) return;
-
             if (pointerInfos.Length < _blockTouchInputThreshold ||
                 Input.PointCapture.Instance.State == Common.Input.CaptureState.CapturingInvalid ||
                 _tempDisable)
@@ -180,55 +170,59 @@ namespace GestureSign.Daemon.Filtration
 
             foreach (var currentPointerInfo in pointerInfos)
             {
-                POINTER_TOUCH_INFO pti = new POINTER_TOUCH_INFO
+                POINTER_INFO pointerInfo = new POINTER_INFO()
                 {
-                    TouchFlags = TOUCH_FLAGS.NONE,
-                    PointerInfo = new POINTER_INFO
-                    {
-                        pointerType = POINTER_INPUT_TYPE.TOUCH,
-                        PtPixelLocation = currentPointerInfo.PtPixelLocation,
-                    }
+                    pointerType = POINTER_INPUT_TYPE.TOUCH,
+                    PtPixelLocation = currentPointerInfo.PtPixelLocation,
                 };
 
                 if (currentPointerInfo.PointerFlags.HasFlag(POINTER_FLAGS.UPDATE))
                 {
-                    pti.PointerInfo.PointerFlags = POINTER_FLAGS.INCONTACT | POINTER_FLAGS.INRANGE | POINTER_FLAGS.UPDATE;
+                    pointerInfo.PointerFlags = POINTER_FLAGS.INCONTACT | POINTER_FLAGS.INRANGE | POINTER_FLAGS.UPDATE;
 
-                    if (_pointerIdList.ContainsKey(currentPointerInfo.PointerID))
+                    if (_pointerIdList.TryGetValue(currentPointerInfo.PointerID, out int id))
                     {
-                        pti.PointerInfo.PointerID = _pointerIdList[currentPointerInfo.PointerID];
+                        pointerInfo.PointerID = id;
                     }
                     else continue;
                 }
                 else if (currentPointerInfo.PointerFlags.HasFlag(POINTER_FLAGS.UP))
                 {
-                    pti.PointerInfo.PointerFlags = POINTER_FLAGS.UP;
+                    pointerInfo.PointerFlags = POINTER_FLAGS.UP;
 
                     upFlagCount++;
 
-                    if (_pointerIdList.ContainsKey(currentPointerInfo.PointerID))
+                    if (_pointerIdList.TryGetValue(currentPointerInfo.PointerID, out int id))
                     {
-                        int id = _pointerIdList[currentPointerInfo.PointerID];
-                        pti.PointerInfo.PointerID = id;
+                        pointerInfo.PointerID = id;
                         _idPool.Enqueue(id);
                         _pointerIdList.Remove(currentPointerInfo.PointerID);
+                        if (_pointerIdList.Count == 0)
+                        {
+                            pointerInfo.PointerFlags |= POINTER_FLAGS.INRANGE;
+                        }
                     }
                     else continue;
                 }
                 else if (currentPointerInfo.PointerFlags.HasFlag(POINTER_FLAGS.DOWN))
                 {
-                    pti.PointerInfo.PointerFlags = POINTER_FLAGS.DOWN | POINTER_FLAGS.INRANGE | POINTER_FLAGS.INCONTACT;
+                    pointerInfo.PointerFlags = POINTER_FLAGS.DOWN | POINTER_FLAGS.INRANGE | POINTER_FLAGS.INCONTACT;
 
                     if (_pointerIdList.ContainsKey(currentPointerInfo.PointerID)) continue;
 
                     if (_idPool.Count > 0)
                     {
-                        pti.PointerInfo.PointerID = _idPool.Dequeue();
-                        _pointerIdList.Add(currentPointerInfo.PointerID, pti.PointerInfo.PointerID);
+                        pointerInfo.PointerID = _idPool.Dequeue();
+                        _pointerIdList.Add(currentPointerInfo.PointerID, pointerInfo.PointerID);
                     }
                 }
                 else continue;
 
+                POINTER_TOUCH_INFO pti = new POINTER_TOUCH_INFO()
+                {
+                    TouchFlags = TOUCH_FLAGS.NONE,
+                    PointerInfo = pointerInfo,
+                };
                 ptis.Add(pti);
             }
 
@@ -242,62 +236,6 @@ namespace GestureSign.Daemon.Filtration
                 }
             }
             return ptis;
-        }
-
-        private bool SimulateDoubleTap(List<POINTER_TOUCH_INFO> ptis)
-        {
-            if (ptis.Count == 1)
-            {
-                if ((ptis[0].PointerInfo.PointerFlags & POINTER_FLAGS.DOWN) == POINTER_FLAGS.DOWN)
-                {
-                    string className;
-                    try
-                    {
-                        className = ManagedWinapi.Windows.SystemWindow.FromPointEx(ptis[0].PointerInfo.PtPixelLocation.X, ptis[0].PointerInfo.PtPixelLocation.Y, true, true).ClassName;
-                    }
-                    catch
-                    {
-                        return false;
-                    }
-                    if (_doubleTapPoint != null && _doubleTapList.Contains(className))
-                    {
-                        var distance = Math.Sqrt(Math.Pow(ptis[0].PointerInfo.PtPixelLocation.X - _doubleTapPoint.Value.X, 2) +
-                            Math.Pow(ptis[0].PointerInfo.PtPixelLocation.Y - _doubleTapPoint.Value.Y, 2));
-                        var deltaTime = Environment.TickCount - _lastTapTime;
-                        if (distance < _doubleTapRadius && deltaTime < _doubleTapTime)
-                        {
-                            _blockTap = true;
-                            Task.Delay(100).ContinueWith((t) =>
-                            {
-                                var infoArray = ptis.ToArray();
-                                infoArray[0].PointerInfo.PointerFlags = POINTER_FLAGS.DOWN | POINTER_FLAGS.INRANGE | POINTER_FLAGS.INCONTACT;
-                                NativeMethods.InjectTouchInput(1, infoArray);
-                                Thread.Sleep(2);
-                                infoArray[0].PointerInfo.PointerFlags = POINTER_FLAGS.UP;
-                                NativeMethods.InjectTouchInput(1, infoArray);
-                                Thread.Sleep(2);
-                                infoArray[0].PointerInfo.PointerFlags = POINTER_FLAGS.DOWN | POINTER_FLAGS.INRANGE | POINTER_FLAGS.INCONTACT;
-                                NativeMethods.InjectTouchInput(1, infoArray);
-                                Thread.Sleep(2);
-                                infoArray[0].PointerInfo.PointerFlags = POINTER_FLAGS.UP;
-                                NativeMethods.InjectTouchInput(1, infoArray);
-                            });
-
-                            _doubleTapPoint = null;
-                            return true;
-                        }
-                    }
-                    _doubleTapPoint = ptis[0].PointerInfo.PtPixelLocation;
-                    _lastTapTime = Environment.TickCount;
-                }
-                else if (_blockTap && (ptis[0].PointerInfo.PointerFlags & POINTER_FLAGS.UP) == POINTER_FLAGS.UP)
-                {
-                    _blockTap = false;
-                    return true;
-                }
-            }
-            else _doubleTapPoint = null;
-            return false;
         }
 
         #endregion ProcessInput
